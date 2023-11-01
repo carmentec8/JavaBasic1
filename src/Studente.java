85f1efb5f9faa4fcb76315dde7cd13b5135fb383
carmentec8 <carmentecchia@gmail.com>
2023-11-01 18:51:50 +0100

    added file

---
+++ src/Studente.java
@@ -0,0 +1,42 @@
+public class Studente {
+    public String nome;
+    public String cognome;
+    public int identificativo;
+
+
+    public Studente(String nome, String cognome, int identificativo){
+    this.nome = nome;
+    this.cognome = cognome;
+    this.identificativo = identificativo;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String setNome(String nome){
+        this.nome = nome;
+        return nome;
+    }
+    public String getCognome(){
+    return cognome;
+    }
+    public String setCognome(String cognome){
+       this.cognome = cognome;
+       return cognome;
+    }
+    public int getIdentificativo(){
+    return identificativo;
+    }
+
+    public void setIdentificativo(int identificativo){
+        this.identificativo = identificativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Studente{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", identificativo=" + identificativo +
+                '}';
+    }
+}
