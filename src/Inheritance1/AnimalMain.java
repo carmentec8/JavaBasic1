44dd56901880491bfdb0e497ad7f410e00f96e84
carmentec8 <carmentecchia@gmail.com>
2023-11-08 15:45:37 +0100

    added file

---
+++ src/Inheritance1/AnimalMain.java
@@ -0,0 +1,10 @@
+package Inheritance1;
+
+public class AnimalMain {
+    public static void main(String[] args) {
+    Animals animal = new Animals();
+    Cat cat = new Cat();
+    animal.soundIs();
+    cat.soundIs();
+}
+}
